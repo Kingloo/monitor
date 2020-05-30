c0dfbf9eb6b530f6eb7e46fb294cd6d9c7bb6590
Kingloo <k1ngloo@hotmail.com>
2020-05-30 07:37:23 +0200

    Add comments explaining some of the values passed to SendMessage

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,13 @@ use winapi::shared::windef::HWND;
 use winapi::um::winuser::SendMessageW;
 
 fn set_all_monitors_to_standby() {
+    // 0xffff sends the message to all top-level windows
     let all_windows: HWND = 0xFFFF as HWND;
     let wm_syscommand: u32 = 0x0112;
     let sc_monitorpower: usize = 0xF170;
+    // MONITOR_ON = -1
+    // MONITOR_STANDBY = 1
+    // MONITOR_OFF = 2
     let monitor_off: isize = 2;
     
     unsafe {
