9d05c9369e059acb4cdc29299eada29a3b670a07
Kingloo <k1ngloo@hotmail.com>
2021-05-11 22:34:53 +0200

    switch from winapi from windows
    Official Windows bindings crate from Microsoft.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
-extern crate winapi;
-
-use std::time::Duration;
 use std::thread::sleep;
-use winapi::shared::windef::HWND;
-use winapi::um::winuser::SendMessageW;
+use std::time::Duration;
+use windows_bindings::Windows::Win32::WindowsAndMessaging::*;
 
-fn set_all_monitors_to_standby() {
-    // 0xffff sends the message to all top-level windows
-    let all_windows: HWND = 0xFFFF as HWND;
-    let wm_syscommand: u32 = 0x0112;
-    let sc_monitorpower: usize = 0xF170;
-    // MONITOR_ON = -1
-    // MONITOR_STANDBY = 1
-    // MONITOR_OFF = 2
-    let monitor_off: isize = 2;
-    
-    unsafe {
-        SendMessageW(all_windows, wm_syscommand, sc_monitorpower, monitor_off)
-    };
-}
+// HWND 0xFFFF sends to all windows
+// 0x0112 is WM_SYSCOMMAND
+// 0xF170 is SC_MONITORPOWER
+// MONITOR_ON = -1
+// MONITOR_STANDBY = 1
+// MONITOR_OFF = 2
 
-fn main() {
-    sleep(Duration::from_secs(1));
-    set_all_monitors_to_standby();
+fn main() -> windows::Result<()> {
+	sleep(Duration::from_secs(1));
+	unsafe {
+		SendMessageW(
+			HWND(0xFFFF),
+			WM_SYSCOMMAND,
+			WPARAM(SC_MONITORPOWER as usize),
+			LPARAM(2),
+		);
+	};
+	Ok(())
 }
